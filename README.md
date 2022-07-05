992155760f11e0b61a3d5e1aba5fd6f7d87a82f0
mrcaseyrobison <104058949+mrcaseyrobison@users.noreply.github.com>
2022-07-05 16:45:01 -0400

    Cleaned up CSS and code

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ A Day Planner made using JQuery, Moment JS and CSS Bootstrap.
 
 Deployed page link: https://mrcaseyrobison.github.io/5.Day-Planner/
 
-+ Initial HTML & CSS elements were already pre-fabricated.
-+ Minor styling tweaks in CSS (Background color, fonts, thicker inner borders in the planner)
-+ I used CSS Bootstrap with pre-defined class & ID tags to create the time blocks from 9am - 5pm (09:00 hours to 17:00 hours)
-+ I used Moment JS to display the current date at the top of the page.
-+ Local storage works!
+- Initial HTML & CSS elements were already pre-fabricated.
+- Minor styling tweaks in CSS (Background color, fonts, thicker inner borders in the planner)
+- I used CSS Bootstrap with pre-defined class & ID tags to create the time blocks from 9am - 5pm (09:00 hours to 17:00 hours)
+- I used Moment JS to display the current date at the top of the page.
+- Local storage works!
 
 <img src = "./images/demo.gif">
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // saveBtn variable
-var button = $(".saveBtn")
+var button = $(".saveBtn");
 
 // Moment JS to display current date (Day of Week / Month / Day of Month )
 var todaysDate = moment().format("dddd MMMM Do");
@@ -8,31 +8,37 @@ console.log(todaysDate);
 
 // Function to show current time block (hour) in red, upcoming hours in green, and past hours in grey.
 function currentTime() {
-    var currentHour = moment().hours()
-    var time = $(".time-block");
-    time.each(function() {
-        var hour = parseInt($(this).attr("id"))
-        // JQuery callback to let initialize highlights
-        if (hour === currentHour) {
-            $(this).children(".col-sm-10").attr("class", "present col-sm-10 description")
-        } else if (currentHour > hour) {
-            $(this).children(".col-sm-10").attr("class", "past col-sm-10 description")
-        } else {
-            $(this).children(".col-sm-10").attr("class", "future col-sm-10 description")
-        }
-    })
-    console.log("Current Hour: " + currentHour)
-};
+  var currentHour = moment().hours();
+  var time = $(".time-block");
+  time.each(function () {
+    var hour = parseInt($(this).attr("id"));
+    // JQuery callback to let initialize highlights
+    if (hour === currentHour) {
+      $(this)
+        .children(".col-sm-10")
+        .attr("class", "present col-sm-10 description");
+    } else if (currentHour > hour) {
+      $(this)
+        .children(".col-sm-10")
+        .attr("class", "past col-sm-10 description");
+    } else {
+      $(this)
+        .children(".col-sm-10")
+        .attr("class", "future col-sm-10 description");
+    }
+  });
+  console.log("Current Hour: " + currentHour);
+}
 
-currentTime()
+currentTime();
 
 // Save to Local Storage
-button.on("click", function(event){
-    var toDo = $(this).siblings(".description").val();
-    var time =$(this).parent().attr("id")
-    localStorage.setItem(time, toDo);
-    console.log(toDo);
-})
+button.on("click", function (event) {
+  var toDo = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, toDo);
+  console.log(toDo);
+});
 
 // Get Items from local storage
 $("#9 .description").val(localStorage.getItem("9"));
